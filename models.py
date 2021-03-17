61fb5c15bc28b25a4792cdf62410bc2e09f79dc4
Kaemaros <64965857+Kaemaros@users.noreply.github.com>
2021-03-17 23:21:47 +0700

    Revert "3/19/2021@11:17"
    This reverts commit 9c4ad0ef58c50f024df74fbaf07374e5ede4aa66.

--- models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy 
-db = SQLAlchemy()
-class aboutusDB(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(50), unique=True, nullable=False)
-    lastname = db.Column(db.String(50), unique=True, nullable=False)
-    std_id = db.Column(db.String(12), unique=True, nullable=False)
-    img_path = db.Column(db.String(120), unique=True, nullable=False)
-
